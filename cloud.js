f72843d6841a0df8eeafa8f25f980ca9a13ff42f
FlashCom Official <132670513+FlashComOfficial@users.noreply.github.com>
2025-11-09 18:17:04 +0530

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -5,7 +5,7 @@ Api.editMessageMedia({
   message_id: request.message.message_id,
   media: {
     type: "photo",
-    media: "https://flashcomcloud.alwaysdata.net/download/1760326010_file_89.jpg",
+    media: "https://flashcomcloud.onelocal.host/download-file/1760326010_file_89.jpg",
     caption: `☁️ *FlashCom Cloud*\n\n• *All Builds Apks Cloud* - All your builds are stored on FlashCom Cloud on your telegram id. You can access the cloud via telegrm bot and view or manage your builds apks or even upload your own files to get own direct links`,
     parse_mode: "Markdown"
   },
@@ -16,4 +16,5 @@ Api.editMessageMedia({
       [{ text: "🔙 Back to Main", callback_data: "/start" }]
     ]
   }
+
 })
